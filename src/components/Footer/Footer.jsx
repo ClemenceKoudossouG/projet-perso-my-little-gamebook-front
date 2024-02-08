23aa7679784e99d2dd5f3ab0165a5ccd33123e83
AugustinienJoey <joey.augustinien@oclock.school>
2024-02-08 09:51:29 +0100

    Header and Footer test

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,12 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Toolbar from '@mui/material/Toolbar';
+import { Fullscreen } from '@mui/icons-material';
 
 export default function Footer() {
   return (
-    <Box sx={{ width: 500 }}>
+    <Box sx={{ flexGrow: 1 }}>
       <BottomNavigation showLabels>
         <BottomNavigationAction label=" À propos" />
         <BottomNavigationAction label="Contact" />
