6b117d96e989839e22717df8e9902d6014d43b47
AugustinienJoey <joey.augustinien@oclock.school>
2024-02-09 16:27:43 +0100

    route sign up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,21 @@ import { ThemeProvider } from '@mui/material/styles';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import theme from '../../styles/theme';
+import SignUpSide from '../SignUp/SignUp';
+import Home from '../Home/Home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        <Header color="primary" />
-        <div />
+      <div className="container">
+        <div>
+          <Header color="primary" />
+        </div>
+        <Routes>
+          <Route path="/SignUpSide" element={<SignUpSide />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
         <Footer />
       </div>
     </ThemeProvider>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuBookRoundedIcon from '@mui/icons-material/MenuBookRounded';
 import { useTheme } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 export default function ButtonAppBar() {
   const theme = useTheme();
@@ -16,13 +17,17 @@ export default function ButtonAppBar() {
       <AppBar position="static" sx={{ bgcolor: theme.palette.primary.main }}>
         <Toolbar>
           <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
-            <MenuBookRoundedIcon />
+            <Link to="/">
+              <MenuBookRoundedIcon />
+            </Link>
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             My Little GameBook
           </Typography>
           <Button color="inherit">Sign In</Button>
-          <Button color="inherit">Sign Up</Button>
+          <Button color="inherit">
+            <Link to="SignUpSide">Sign Up</Link>
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
