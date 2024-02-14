c15f952920913ec31f6fad0f54efbbf9ad3d65b4
AugustinienJoey <joey.augustinien@oclock.school>
2024-02-14 14:25:27 +0100

    page test game

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box';
+import './Footer.scss';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import { useTheme } from '@mui/material/styles';
@@ -7,10 +8,24 @@ export default function Footer() {
   const theme = useTheme();
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <BottomNavigation showLabels>
-        <BottomNavigationAction label=" À propos" />
-        <BottomNavigationAction label="Contact" />
+    <Box
+      sx={{
+        flexGrow: 1,
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        bgcolor: theme.palette.primary.main,
+      }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        className="footer"
+        sx={{ bgcolor: theme.palette.primary.main }}
+      >
+        <BottomNavigationAction label=" À propos" sx={{ color: 'white' }} />
+        <BottomNavigationAction label="Contact" sx={{ color: 'white' }} />
       </BottomNavigation>
     </Box>
   );
--- src/components/Game1/Game1.jsx
+++ src/components/Game1/Game1.jsx
@@ -2,8 +2,45 @@ import './Game1.scss';
 
 function Game1() {
   return (
-    <div>
-      <h1>GAME 1 !!</h1>
+    <div
+      className="image-container"
+      style={{ backgroundImage: `url(${imageURL})`, position: 'relative' }}
+    >
+      <img
+        src="/path/to/overlay-image.png"
+        alt="Overlay"
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 1,
+        }}
+      />
+
+      <p
+        style={{
+          position: 'absolute',
+          top: '20px',
+          left: '20px',
+          color: 'white',
+          zIndex: 2,
+        }}
+      >
+        Votre texte ici
+      </p>
+
+      <div
+        style={{
+          position: 'absolute',
+          bottom: '20px',
+          right: '20px',
+          zIndex: 2,
+        }}
+      >
+        <button style={{ marginRight: '10px' }}>Action 1</button>
+        <button>Action 2</button>
+      </div>
     </div>
   );
 }
