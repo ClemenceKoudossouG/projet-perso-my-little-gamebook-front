c60cdb6798b21fe8e7bc74d30afabc99932a3f38
ClemenceKoudossouG <clemence.garde@icloud.com>
2025-03-13 16:11:52 +0100

    fixed material ui dependency issue

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Container, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Container, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles(() => ({
   root: {
