c44a0bbaf2ee4d1967e77754adeb20c8b365d3a5
ClemenceKoudossouG <clemence.garde@icloud.com>
2025-03-17 17:47:44 +0100

    fixed env variables config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@scssVariables': '/src/styles/_variables.scss',
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@scssVariables': path.resolve(__dirname, 'src/styles/_variables.scss'),
+      },
+      extensions: ['.js', '.jsx', '.scss', '.css'],
     },
-    extensions: ['.js', '.jsx', '.scss', '.css'],
-  },
+    define: {
+      'process.env': env,
+    },
+  };
 });
